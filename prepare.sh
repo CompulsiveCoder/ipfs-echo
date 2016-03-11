35871a3bc7f9e993f331c988c76991d0ada33536
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-11 12:54:40 +1100

    Added scripts

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y git wget mono-complete mono-xsp4 unzip
+
+wget -q https://raw.githubusercontent.com/ipfs/install-go-ipfs/master/install-ipfs.sh
+sudo sh install-ipfs.sh
+
+mozroots --import --sync
+
