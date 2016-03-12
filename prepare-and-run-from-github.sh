1d7ec6a027054bdceb7eafd901c8588a18b126c9
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 12:42:31 +1100

    Script updates

--- prepare-and-run-from-github.sh
+++ prepare-and-run-from-github.sh
@@ -4,6 +4,8 @@ if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 
+echo "Branch: $BRANCH"
+
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/$BRANCH/prepare.sh | sh
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/$BRANCH/run-from-github.sh | sh
 
--- run-from-github-via-docker.sh
+++ run-from-github-via-docker.sh
@@ -1,1 +1,7 @@
-docker run -it compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/master/run-from-github.sh | sh"
+BRANCH=$1
+
+if [ -z "$BRANCH" ]; then
+    BRANCH="master"
+fi
+
+docker run -it compulsivecoder/ubuntu-mono-ipfs /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/$BRANCH/run-from-github.sh | sh"
