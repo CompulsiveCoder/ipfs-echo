6928538e959a16a737e33b473c415492226f3827
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 13:45:32 +1100

    Fixed more scripts

--- prepare-and-test-from-github.sh
+++ prepare-and-test-from-github.sh
@@ -4,5 +4,5 @@ if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 
-curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/prepare.sh | sh
-curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/test-from-github.sh | sh -s $BRANCH
+curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/$BRANCH/prepare.sh | sh
+curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/$BRANCH/test-from-github.sh | sh -s $BRANCH
