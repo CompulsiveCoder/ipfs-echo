d442a684f35c47f31dab3116509a90ca8f66d604
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-12 13:44:26 +1100

    Fixed script

--- test-from-github-via-docker.sh
+++ test-from-github-via-docker.sh
@@ -4,4 +4,4 @@ if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 
-docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/test-from-github.sh | sh -s $BRANCH"
+docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-echo/$BRANCH/test-from-github.sh | sh -s $BRANCH"
