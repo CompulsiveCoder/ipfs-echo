ab5aa3ef3e974b5b2b7ce5e0718aadd22f4e86ad
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-17 21:30:12 +1100

    Reorganized tests

--- src/ipfs.echo.Core.Tests.Integration/ipfsEchoIntegrationTestFixture.cs
+++ src/ipfs.echo.Core.Tests.Integration/ipfsEchoIntegrationTestFixture.cs
@@ -1,52 +1,26 @@
 ﻿using System;
 using NUnit.Framework;
-using System.Threading;
-using ipfs.echo.Core;
-using ipfs.Core.Tests;
-using ipfs.Core.Tests.Integration;
 using ipfs.Core;
 
-namespace ipfs.echo.Core.Tests
+namespace ipfs.echo.Core.Tests.Integration
 {
 	[TestFixture(Category="Integration")]
-	public class ipfsEchoIntegrationTestFixture : BaseIntegrationTestFixture
+	public class ipfsEchoTestFixture
 	{
-		// TODO: Overhaul this test and re-enable
-		//[Test]
-		public void Test_Publish()
+		public ipfsEchoTestFixture ()
 		{
-			new DockerTestLauncher ().Launch (this);
 		}
 
-		public override void Execute()
+		[Test]
+		public void Test_Echo()
 		{
+			var client = new ipfsClient ();
+			client.Init ();
 
 			var echo = new ipfsEcho ();
-			echo.IsVerbose = true;
-			echo.Init ();
-				
-			var firstString = "one";
-
-			var subFolderName = Guid.NewGuid().ToString();
-
-			var peerId = echo.Echo (firstString, subFolderName);
-
-			Console.WriteLine(peerId);
-
-			var fileChecker = new ipfsFileChecker ();
-
-			var relativeFilePath = subFolderName + "/data.txt";
-
-			fileChecker.CheckTestFile ("ipns", peerId, relativeFilePath, firstString);
-
-			var secondString = "two";
-
-			peerId = echo.Echo (secondString, subFolderName);
-
-			var combinedString = firstString + Environment.NewLine + secondString;
-
-			fileChecker.CheckTestFile ("ipns", peerId, relativeFilePath, combinedString);
+			Console.WriteLine(echo.Echo ("Hello world!"));
 		}
+
 	}
 }
 
---
+++ src/ipfs.echo.Core.Tests.Integration/ipfsEchoPublishIntegrationTestFixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using System.Threading;
+using ipfs.echo.Core;
+using ipfs.Core.Tests;
+using ipfs.Core.Tests.Integration;
+using ipfs.Core;
+
+namespace ipfs.echo.Core.Tests.Integration
+{
+	[TestFixture(Category="Integration")]
+	public class ipfsEchoPublishIntegrationTestFixture : BaseIntegrationTestFixture
+	{
+		// TODO: Overhaul this test and re-enable
+		//[Test]
+		public void Test_Publish()
+		{
+			new DockerTestLauncher ().Launch (this);
+		}
+
+		public override void Execute()
+		{
+
+			var echo = new ipfsEcho ();
+			echo.IsVerbose = true;
+			echo.Init ();
+				
+			var firstString = "one";
+
+			var subFolderName = Guid.NewGuid().ToString();
+
+			var peerId = echo.Echo (firstString, subFolderName);
+
+			Console.WriteLine(peerId);
+
+			var fileChecker = new ipfsFileChecker ();
+
+			var relativeFilePath = subFolderName + "/data.txt";
+
+			fileChecker.CheckTestFile ("ipns", peerId, relativeFilePath, firstString);
+
+			var secondString = "two";
+
+			peerId = echo.Echo (secondString, subFolderName);
+
+			var combinedString = firstString + Environment.NewLine + secondString;
+
+			fileChecker.CheckTestFile ("ipns", peerId, relativeFilePath, combinedString);
+		}
+	}
+}
+
