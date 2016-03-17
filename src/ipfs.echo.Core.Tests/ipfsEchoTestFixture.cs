3d744e4b39a948dcd3ed574d7014e25d52fff032
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-17 21:24:19 +1100

    Added missing file

---
+++ src/ipfs.echo.Core.Tests/ipfsEchoTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ipfs.echo.Core.Tests
+{
+	[TestFixture(Category="Unit")]
+	public class ipfsEchoTestFixture
+	{
+		public ipfsEchoTestFixture ()
+		{
+		}
+
+		[Test]
+		public void Test_Echo()
+		{
+			var echo = new ipfsEcho ();
+			Console.WriteLine(echo.Echo ("Hello world!"));
+		}
+
+	}
+}
+
