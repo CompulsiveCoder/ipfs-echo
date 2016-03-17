6356dceb4b3fd259db559bc40470eb5ea629d0b6
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-18 09:44:57 +1100

    Attempting to speed up the build

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -10,4 +10,9 @@ fi
 
 mono nuget.exe install nunit -version 2.6.4
 mono nuget.exe install nunit.runners -version 2.6.4
-mono nuget.exe install newtonsoft.json
+mono nuget.exe install newtonsoft.json -version 8.0.3
+
+# Copy libraries to the submodule to speed up the build
+cp nuget.exe ../mod/ipfs-cs/lib
+cp NUnit.2.6.4/ ../mod/ipfs-cs/lib -r
+cp NUnit.Runners.2.6.4/ ../mod/ipfs-cs/lib -r
