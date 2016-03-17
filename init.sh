6868834fce47c2a42cb71e768e750b3b9552f547
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-18 09:14:12 +1100

    Added check in init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,17 @@
 DIR=$PWD
 
 echo "Initializing ipfs-echo project"
+echo "Dir: $PWD"
 
 cd mod/ipfs-cs/
-sh init.sh
-cd $DIR
+INIT_FILE="init.sh"
+if [ ! -f "$INIT_FILE" ]; then
+  echo "ipfs-cs init file not found: $PWD/$INIT_FILE. Did the submodule fail to initialize?"
+else
+  sh init.sh
+  cd $DIR
 
-cd lib
-sh get-libs.sh
-cd $DIR
+  cd lib
+  sh get-libs.sh
+  cd $DIR
+fi
